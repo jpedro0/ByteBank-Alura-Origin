3fda3301b07ae6b6561fcc96fbb2363f265a842a
Joao Pedro Brito <jpedro0brito@gmail.com>
2020-03-08 18:15:03 -0400

    [TSK] Integrando o Contact List no SQLLITE [REF] Ajeitando o widget para receber a lista de contanto [REF] Coloando FutureBuilder etc..

--- lib/screens/contact/list.dart
+++ lib/screens/contact/list.dart
@@ -1,4 +1,7 @@
+import 'package:bytebankorigin/database/app_database.dart';
+import 'package:bytebankorigin/models/Contact.dart';
 import 'package:bytebankorigin/screens/contact/form.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class ContectsList extends StatelessWidget {
@@ -8,21 +11,39 @@ class ContectsList extends StatelessWidget {
       appBar: AppBar(
         title: Text('Contects'),
       ),
-      body: ListView(
-        children: <Widget>[
-          Card(
-            child: ListTile(
-              title: Text(
-                'teste',
-                style: TextStyle(fontSize: 24.0),
-              ),
-              subtitle: Text(
-                '1000',
-                style: TextStyle(fontSize: 16.0),
-              ),
-            ),
-          )
-        ],
+      body: FutureBuilder(
+        future: findAll(),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+              break;
+            case ConnectionState.waiting:
+              return Center(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    CircularProgressIndicator(),
+                    Text('Loading'),
+                  ],
+                ),
+              );
+              break;
+            case ConnectionState.active:
+              break;
+            case ConnectionState.done:
+              final List<Contact> contacts = snapshot.data;
+              return ListView.builder(
+                itemCount: contacts.length,
+                itemBuilder: (context, index) {
+                  final contact = contacts[index];
+                  return _ContactItem(contact);
+                },
+              );
+              break;
+          }
+          return Text('data');
+        },
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
@@ -31,6 +52,28 @@ class ContectsList extends StatelessWidget {
           );
         },
         child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class _ContactItem extends StatelessWidget {
+  final Contact contact;
+
+  _ContactItem(this.contact);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        title: Text(
+          contact.name,
+          style: TextStyle(fontSize: 24.0),
+        ),
+        subtitle: Text(
+          contact.accountNumber.toString(),
+          style: TextStyle(fontSize: 16.0),
+        ),
       ),
     );
   }
