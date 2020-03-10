3ab7ac2f452bff95618c9e09267ce1a06079e583
Joao Pedro Brito <jpedro0brito@gmail.com>
2020-03-10 15:14:03 -0400

    [TSK] Refatorando o Dachboard [ [REF] trocando o texto e o icone do card e tambem ajeitando o sroll dele para ir para direita [NEW] Adicionando mais uma tela, de transacao [REF] e tambem colocando o navigation no nova tela

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -1,4 +1,5 @@
 import 'package:bytebankorigin/screens/contact/list.dart';
+import 'package:bytebankorigin/screens/transactions_list.dart';
 import 'package:flutter/material.dart';
 
 class Dashboard extends StatelessWidget {
@@ -16,43 +17,82 @@ class Dashboard extends StatelessWidget {
             padding: const EdgeInsets.all(8.0),
             child: Image.asset('images/bytebank_logo.png'),
           ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Material(
-              color: Theme.of(context).primaryColor,
-              child: InkWell(
-                onTap: () {
-                  Navigator.of(context).push(MaterialPageRoute(
-                    builder: (context) => ContectsList(),
-                  ));
-                },
-                child: Container(
-                  padding: const EdgeInsets.all(8.0),
-                  height: 100,
-                  width: 150,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Icon(
-                        Icons.people,
-                        color: Colors.white,
-                        size: 24.0,
-                      ),
-                      Text(
-                        'Teste',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 16.0,
-                        ),
-                      )
-                    ],
-                  ),
+          Container(
+            height: 120,
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: <Widget>[
+                _FeatureItem(
+                  Icons.monetization_on,
+                  'Transfer',
+                  onClick: () => _showContactsList(context),
                 ),
-              ),
+                _FeatureItem(
+                  Icons.description,
+                  'Transaction Feed',
+                  onClick: () => _showTransactionsList(context),
+                ),
+              ],
             ),
           )
         ],
+      ),
+    );
+  }
+
+  void _showContactsList(BuildContext context) {
+    Navigator.of(context).push(MaterialPageRoute(
+      builder: (context) => ContectsList(),
+    ));
+  }
+
+  void _showTransactionsList(BuildContext context) {
+    Navigator.of(context).push(MaterialPageRoute(
+      builder: (context) => TransactionsList(),
+    ));
+  }
+}
+
+class _FeatureItem extends StatelessWidget {
+  final IconData _icone;
+  final String _name;
+  final Function onClick;
+
+  _FeatureItem(this._icone, this._name, {@required this.onClick})
+      : assert(_icone != null), assert(onClick != null);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Material(
+        color: Theme.of(context).primaryColor,
+        child: InkWell(
+          onTap: () => onClick(),
+          child: Container(
+            padding: const EdgeInsets.all(8.0),
+            height: 100,
+            width: 150,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Icon(
+                  _icone,
+                  color: Colors.white,
+                  size: 24.0,
+                ),
+                Text(
+                  _name,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16.0,
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
