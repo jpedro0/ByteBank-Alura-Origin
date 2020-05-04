a94fefdcd0e11ca2e8121b04a065cb45fb9c3402
JPedro Brito <jpedro0brito@gmail.com>
2020-05-03 23:01:51 -0400

    Idempotência ao enviar transferências

--- lib/http/webclients/transaction_webclient.dart
+++ lib/http/webclients/transaction_webclient.dart
@@ -16,12 +16,20 @@ class TransactionWebClient {
       return Transaction.fromJson(jsonDecode(response.body));
     }
 
-    HttpException(_statusCodeResponses[response.statusCode]);
+    HttpException(_getMessage(response.statusCode));
+  }
+
+  String _getMessage(int statusCode){
+    if(_statusCodeResponses.containsKey(statusCode)){
+      return _statusCodeResponses[statusCode];
+    }
+    return 'Unknown error';
   }
 
   static final Map<int, String> _statusCodeResponses = {
     400: 'there was an error submitting transaction',
-    401: 'authentication failed'
+    401: 'authentication failed',
+    409: 'transaction always exists'
   };
 
   Future<List<Transaction>> findAll() async {
--- lib/models/transaction.dart
+++ lib/models/transaction.dart
@@ -1,25 +1,29 @@
 import 'Contact.dart';
 
 class Transaction {
+  final String id;
   final double value;
   final Contact contact;
 
   Transaction(
       this.value,
       this.contact,
+      this.id,
       );
 
   @override
   String toString() {
-    return 'Transaction{value: $value, contact: $contact}';
+    return 'Transaction{id: $id value: $value, contact: $contact}';
   }
 
   Transaction.fromJson(Map<String, dynamic> json)
-    : value = json['value'],
+    : id = json['id'],
+      value = json['value'],
       contact = Contact.fromJson(json['contact']);
 
   Map<String, dynamic> toJson() =>
   {
+    'id': id,
     'value': value,
     'contact': contact.toJson(),
   };
