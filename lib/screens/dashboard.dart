3f2b05c6d9c41923e8106f05454f4cb619303dec
JPedro Brito <jpedro0brito@gmail.com>
2020-05-13 23:52:11 -0400

    corrigindo erro do layout dasboard para nao quebra nos teste

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -9,33 +9,42 @@ class Dashboard extends StatelessWidget {
       appBar: AppBar(
         title: Text('Dashboard'),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Image.asset('images/bytebank_logo.png'),
-          ),
-          Container(
-            height: 120,
-            child: ListView(
-              scrollDirection: Axis.horizontal,
+      body: LayoutBuilder(
+        builder: (context, constraints) => SingleChildScrollView(
+          child: ConstrainedBox(
+            constraints: BoxConstraints(
+              minHeight: constraints.maxHeight
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
-                FeatureItem(
-                  Icons.monetization_on,
-                  'Transfer',
-                  onClick: () => _showContactsList(context),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Image.asset('images/bytebank_logo.png'),
                 ),
-                FeatureItem(
-                  Icons.description,
-                  'Transaction Feed',
-                  onClick: () => _showTransactionsList(context),
-                ),
+                Container(
+                  height: 120,
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    children: <Widget>[
+                      FeatureItem(
+                        Icons.monetization_on,
+                        'Transfer',
+                        onClick: () => _showContactsList(context),
+                      ),
+                      FeatureItem(
+                        Icons.description,
+                        'Transaction Feed',
+                        onClick: () => _showTransactionsList(context),
+                      ),
+                    ],
+                  ),
+                )
               ],
             ),
-          )
-        ],
+          ),
+        ),
       ),
     );
   }
